63c88fd15e963c58e6a835e7f0f6da73f7b0db62
J. Smeets <16883019+juulSme@users.noreply.github.com>
2025-01-05 00:21:02 +0100

    Fix missing :crypto application error

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule ModuleStore.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :crypto]
     ]
   end
 
